50e6e9b22677600f0faa5a2d63020219aac94b30
Arpithasaireddy <arpitha.dpearl@gmail.com>
2016-02-17 15:11:50 -0600

    first commit

--- program2/program2/src/program2/first.java
+++ program2/program2/src/program2/first.java
@@ -1,26 +1,56 @@
 package program2;
 
-public class first 
-{
-	
-		  public static void main(String[] args)
-		   {
-		     int count = 0;
-		     for(int i=1; i<=1000000; i = i+1)
-			{
+public class first {
 
-		  	   if((i%9==0)||(i%13==0)||(i%27==0)||(i%81==0))
-				{
-			
-					count= count + 1;
-					}
+	public static void main(String[] args) {
+		String s = "hey 123 my world";
+
+		System.out.println("print vowels: ");
+		printVowels(s);
+
+		System.out.println("print consonents");
+		printConsonents(s);
+
+		System.out.println("print digits");
+		printDigits(s);
+	}
+
+	static void printVowels(String s) {
+
+		for (int i = 0; i < s.length(); i++) {
+
+			if ((s.charAt(i) == 'a') || (s.charAt(i) == 'e')
+					|| (s.charAt(i) == 'i') || (s.charAt(i) == 'o')
+					|| (s.charAt(i) == 'u'))
+
+				System.out.println(s.charAt(i));
 		}
+	}
 
-					
-						System.out.println(count);
+	static void printConsonents(String s) {
+		for (int i = 0; i < s.length(); i++) {
 
-			}
-			
-			
+			if ((s.charAt(i) != 'a') && (s.charAt(i) != 'e')
+					&& (s.charAt(i) != 'i') && (s.charAt(i) != 'o')
+					&& (s.charAt(i) != 'u'))
 
+				System.out.println(s.charAt(i));
+		}
+	}
+
+	static void printDigits(String s) {
+
+		for (int i = 0; i < s.length(); i++) {
+
+			if ((s.charAt(i) == '0') || (s.charAt(i) == '1')
+					|| (s.charAt(i) == '2') || (s.charAt(i) == '3')
+					|| (s.charAt(i) == '4') || (s.charAt(i) == '5')
+					|| (s.charAt(i) == '6') || (s.charAt(i) == '7')
+					|| (s.charAt(i) == '8') || (s.charAt(i) == '9'))
+
+				System.out.println(s.charAt(i));
+		}
+	}
 }
+
+	
